f3dddb4969f21244fe229466c713db9bc1694745
Sangwon Park <harin.luna@gmail.com>
2019-12-25 16:46:01 +0900

    Update BOJ_11728.cpp
    배열 입력 부분에서 Segmentation fault 뜬다.

--- 분할정복/BOJ_11728.cpp
+++ 분할정복/BOJ_11728.cpp
@@ -3,62 +3,67 @@
 
 using namespace std;
 
-void input(int iter, queue<int>&seq)
+vector<int> temp;
+
+void merge(vector<int> seq, int begin, int mid, int end)
 {
-	int n;
+	int left = begin, m = mid+1, i = begin, l;
 	
-	for(int i=0; i<iter; ++i)
+	while(left<=mid && m<=end)
 	{
-		cin>>n;
-		seq.push(n);
+		if(seq[left]<=seq[m])
+			temp[i++] = seq[left++];
+		else
+			temp[i++] = seq[m++];
+	}
+	
+	if(left>mid)
+		for(l=m; l<=end; l++)
+			temp[i++]=seq[l];
+	
+	else
+		for(l=left; l<=mid; l++)
+			temp[i++]=seq[l];
+	
+	for(l=begin; l<=end; l++)
+		seq[l] = temp[l];
+}
+
+void sort(vector<int> seq, int begin, int end)
+{
+	int mid;
+	
+	if(begin<end)
+	{
+		mid = (begin+end)/2;
+		sort(seq, begin, mid);
+		sort(seq, mid+1, end);
+		merge(seq, begin, mid, end);
 	}
 }
 
 int main(void)
 {
-	int a,b, min;
+	int a,b,n;
 	
 	cin>>a>>b;
 	
-	min = (a>b)? b:a;
+	vector<int> seq(a+b);
 	
-	queue<int> a_seq;
-	queue<int> b_seq;
-	
-	input(a,a_seq);
-	input(b,b_seq);
-	
-	for(int i=0; i<min*2; ++i)
-	{	
-		if(!(a_seq.empty()) && a_seq.front()<b_seq.front() || b_seq.empty())
-		{
-			cout<<a_seq.front()<<" ";
-			a_seq.pop();
-		}
-		else
-		{
-			cout<<b_seq.front()<<" ";
-			b_seq.pop();
-		}
-	}
-	
-	if(a_seq.empty())
+	for(int i=0; i<a+b; ++i)
 	{
-		while(!(b_seq.empty()))
-		{
-			cout<<b_seq.front()<<" ";
-			b_seq.pop();
-		}
+		cin>>n;
+		seq[i]=n;
+		cout<<seq[i]<<" ";
 	}
 	
-	else if(b_seq.empty())
+	for(int i=0; i<a+b; ++i)
 	{
-		while(!(a_seq.empty()))
-		{
-			cout<<a_seq.front()<<" ";
-			a_seq.pop();
-		}
+		cout<<seq[i]<<" ";
 	}
 	
-	return 0;
+	sort(seq,0,a+b);
+	
+	for(int i=0; i<a+b; ++i)
+		cout<<seq[i]<<" ";
 }
