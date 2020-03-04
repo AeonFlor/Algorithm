3859f834d7816e22abd9ffda3ddc986f1afbf223
Sangwon Park <harin.luna@gmail.com>
2020-03-04 17:09:09 +0900

    Update BOJ_10798.cpp

--- 구현/BOJ_10798.cpp
+++ 구현/BOJ_10798.cpp
@@ -24,3 +24,9 @@ int main(void)
 		}
 	}
 }
+
+// printf 와 scanf 로도 풀 수 있었다.
+// scanf 로만 풀 경우 char arr[5][16] 으로 선언하고 scanf("%s", arr[i]); 로 for 문 돌리면 됐다.
+// 그리고 printf 의 경우 scanf 를 쓸 때 마지막에 자동으로 null 이 들어가므로 if(arr[j][i]) 있는지 판단하면 된다.
+
+// 이렇게 되면 vector 를 쓰지않고 string 배열로만 풀 수 있었다.
