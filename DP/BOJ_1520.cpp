576d6fdf85b1ce692400903bb8647af969508e67
Sangwon Park <harin.luna@gmail.com>
2020-03-31 23:07:35 +0900

    Update BOJ_1520.cpp
    오랜만에 알고리즘한다. 흠.. 머리가 좋아진 것 같다 히힣
    근데 다른 문제에 코드 넣어놓고 왜 안 되지 않고 있었다 ㅋㅋ

--- DP/BOJ_1520.cpp
+++ DP/BOJ_1520.cpp
@@ -1,41 +1,54 @@
 #include <cstdio>
-#include <functional>
+#include <cstring>
 
 using namespace std;
 
 int cache[501][501];
-int coor[501][501], M, N;
-
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+int coor[501][501],M,N;
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
 
 int count(int x, int y)
 {
 	int& ret = cache[x][y];
 	int result = 0;
 	
-	if(ret != -1)
-		return ret;
-	
-	if(x==N && y==M)
-		return 1;
-	
-	for(int i=0; i<4; ++i)
+	if(ret!=-1)
 	{
-		if(coor[x][y] > coor[x+dx[i]][y+dy[i]])
-			result += count(x+dx[i], y+dy[i]);
-		printf("%d(%d,%d) -> %d(%d,%d) : %d\n",coor[x][y],x,y,coor[x+dx[i]][y+dy[i]],x+dx[i],y+dy[i],result);
+		printf("JUMP!\n");
+		return ret;
 	}
 	
-	return ret = result;
+	if(x==M && y==N)
+	{
+		printf("FIND FLAG!\n");
+		return 1;
+	}
+	/*
+	if(x==0 || x==M+1 || y==0 || y==N+1)
+	{
+		printf("EXCEPTION!! RETURN! ->");
+		return 0;
+	}
+	*/
+	for(int i=0; i<4; ++i)
+	{
+		if(coor[x][y]>coor[x+dx[i]][y+dy[i]] && !(x+dx[i]==0 || x+dx[i]==M+1 || y+dy[i]==0 || y+dy[i]==N+1))
+		{
+			printf("(%d, %d) -> ", x+dx[i], y+dy[i]);
+			result += count(x+dx[i], y+dy[i]);
+		}
+	}
+	
+	return ret=result;
 }
 
 int main(void)
 {
-	fill(&coor[0][0], &coor[0][0]+sizeof(coor)/sizeof(int),501);
-	fill(&cache[0][0], &cache[0][0]+sizeof(cache)/sizeof(int),-1);
+	memset(coor,501,sizeof(coor));
+	memset(cache, -1, sizeof(cache));
 	
-	scanf("%d %d",&M, &N);
+	scanf("%d %d", &M, &N);
 	
 	for(int i=1; i<=M; ++i)
 		for(int j=1; j<=N; ++j)
@@ -45,11 +58,3 @@ int main(void)
 	
 	return 0;
 }
-
-/*
-4 5
-50 45 37 32 30
-35 50 40 20 25
-30 30 25 17 28
-27 24 22 15 10
-*/
