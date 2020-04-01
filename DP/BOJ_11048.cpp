c2b352415936d8a371c7bf29da19dd30998e555e
Sangwon Park <harin.luna@gmail.com>
2020-04-01 09:52:21 +0900

    Update BOJ_11048.cpp

--- DP/BOJ_11048.cpp
+++ DP/BOJ_11048.cpp
@@ -10,28 +10,16 @@ int cache[1001][1001];
 
 int count(int x, int y)
 {
-	
-	printf("(%d, %d) -> ",x,y);
-	
 	if(x>N || y>M)
-	{
-		printf("OUT\n");
 		return -99999999;
-	}
 	
 	if(x==N && y==M)
-	{
-		printf("FIND FLAG!\n");
 		return plane[x][y];
-	}
 	
 	int & ret = cache[x][y];
 	
 	if(ret!=-1)
-	{
-		printf("JUMP!\n");
 		return ret;
-	}
 	
 	return ret = plane[x][y] + max(max(count(x+1,y), count(x,y+1)), count(x+1,y+1));
 }
