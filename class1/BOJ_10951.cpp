af3214c91440de1dd6bfb18f78938a1d6fe44711
Sangwon Park <harin.luna@gmail.com>
2020-07-05 17:32:45 +0900

    Create BOJ_10951.cpp

---
+++ class1/BOJ_10951.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int A,B;
+	
+	while(cin>>A>>B)
+		cout<<A+B<<'\n';
+	
+	return 0;
+}
