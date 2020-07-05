8a57a63abbaf987c3c00efd93e503a29f757c709
Sangwon Park <harin.luna@gmail.com>
2020-07-05 17:03:51 +0900

    Create BOJ_10869.cpp

---
+++ class1/BOJ_10869.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int A,B;
+	
+	cin>>A>>B;
+	
+	cout<<A+B<<'\n';
+	cout<<A-B<<'\n';
+	cout<<A*B<<'\n';
+	cout<<A/B<<'\n';
+	cout<<A%B<<'\n';
+	
+	return 0;
+}
