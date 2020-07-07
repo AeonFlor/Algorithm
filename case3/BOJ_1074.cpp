4d8e051d3e72c3b669f173af18516732f65a5f44
Sangwon Park <harin.luna@gmail.com>
2020-07-07 22:14:02 +0900

    Update BOJ_1074.cpp

--- case3/BOJ_1074.cpp
+++ case3/BOJ_1074.cpp
@@ -6,22 +6,16 @@ using namespace std;
 int dx[4] = {0,0,1,0};
 int dy[4] = {0,1,-1,1};
 int N,r,c,cnt=-1;
+bool isFind=false;
 
 void recursive(int length, int x, int y)
 {
-	cout<<"recursive("<<length<<", "<<x<<", "<<y<<")\n";
-		
 	if(length == 1)
 	{
 		cnt++;
 		
 		if(x==r && y==c)
-		{
-			cout<<"Find it!!! -> "<<cnt<<'\n';
-			return;
-		}
-		
-		cout<<"?\n";
+			isFind=true;
 	}
 	
 	else
@@ -30,14 +24,18 @@ void recursive(int length, int x, int y)
 		{
 			x += length/2 * dx[i%4];
 			y += length/2 * dy[i%4];
-
-			recursive(length/2,x,y);
+			
+			if(!isFind)
+				recursive(length/2,x,y);
 		}
 	}
 }
 
 int main(void)
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
 	cin>>N>>r>>c;
 	recursive(pow(2,N),0,0);
 	
