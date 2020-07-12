2579c0d09a75657cc209732b27c2059628f1d78e
Sangwon Park <harin.luna@gmail.com>
2020-07-12 17:42:21 +0900

    Update BOJ_2751.cpp

--- class2/BOJ_2751.cpp
+++ class2/BOJ_2751.cpp
@@ -1,49 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
-void quick_sort(vector<int>& arr, int s, int e)
-{
-	int pivot = arr[s];
-	int bs = s, be = e;
-	int temp;
-	
-	while(s<e)
-	{
-		while(pivot<=arr[e] && s<e)
-			e--;
-		
-		if(s>e)
-			break;
-		
-		while(pivot>=arr[s] && s<e)
-			s++;
-		
-		if(s>e)
-			break;
-		
-		temp = arr[s];
-		arr[s] = arr[e];
-		arr[e] = temp;
-	}
-	
-	temp = arr[s];
-	arr[s] = arr[bs];
-	arr[bs] = temp;
-	
-	if(bs<s)
-		quick_sort(arr,bs,s-1);
-	if(be>e)
-		quick_sort(arr,s+1,be);
-}
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	int N,temp,min;
+	int N;
 	cin>>N;
 	
 	vector<int> arr(N);
@@ -51,7 +17,7 @@ int main(void)
 	for(int i=0; i<N; ++i)
 		cin>>arr[i];
 	
-	quick_sort(arr,0,N-1);
+	sort(arr.begin(),arr.end());
 	
 	for(int i=0; i<N; ++i)
 		cout<<arr[i]<<'\n';
