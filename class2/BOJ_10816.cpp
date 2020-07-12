9b71e293176ded044d3b24936376ee61dfab3e92
Sangwon Park <harin.luna@gmail.com>
2020-07-12 23:07:05 +0900

    Update BOJ_10816.cpp

--- class2/BOJ_10816.cpp
+++ class2/BOJ_10816.cpp
@@ -8,8 +8,7 @@ int main(void)
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	int N,M,input,cnt;
-	int left, mid, right;
+	int N,M,input;
 	
 	cin>>N;
 	int cards[N];
@@ -19,34 +18,11 @@ int main(void)
 	
 	sort(cards,cards+N);
 	
-	for(int i=0; i<N; ++i)
-		cout<<cards[i]<<" ";
-	cout<<'\n';
-	
 	cin>>M;
 	for(int i=0; i<M; ++i)
 	{
 		cin>>input;
-		cnt=0;
-		left = 0;
-		right = N-1;
-		mid = N/2;
-		
-		while(right-left>=0)
-		{
-			if(cards[mid]<input)
-				left=mid+1;
-			else if(cards[mid]>input)
-				right=mid-1;
-			else
-				break;
-			mid = right+left/2;
-		}
-		
-		for(int j=mid; cards[j]==cards[mid]; ++j)
-			++cnt;
-		
-		cout<<cnt<<" ";
+		cout<<upper_bound(cards, cards+N, input)-lower_bound(cards, cards+N, input)<<" ";
 	}
 	cout<<'\n';
 	
