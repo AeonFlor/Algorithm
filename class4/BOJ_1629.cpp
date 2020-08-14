fee03bd18d2f5295a82edeaa79e872d6e8b1eee8
Sangwon Park <harin.luna@gmail.com>
2020-08-14 12:37:20 +0900

    Update BOJ_1629.cpp

--- class4/BOJ_1629.cpp
+++ class4/BOJ_1629.cpp
@@ -13,21 +13,13 @@ int main(void)
 	{
 		a=1;
 		for(; b!=0; b=b>>1)
-		{	
-			int i = b%2;
-			
-			if(i==1)
-			{
+		{
+			if(b%2)
 				a = a * exp_res % c;
-				cout<<"ANS : "<<a<<'\n';
-			}
-
-			cout<<"-> EXP_RES : "<<exp_res<<'\n';
+			
 			exp_res = exp_res * exp_res % c;
 		}
 	}
 	
 	cout<<a%c<<'\n';
 }
-
-// 비트연산자 활용, 모듈러
