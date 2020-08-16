e5a57bc068e16c8248b5f92907874437ecc4e95a
Sangwon Park <harin.luna@gmail.com>
2020-08-16 23:55:56 +0900

    시간 초과..?

--- class3/BOJ_11279.cpp
+++ class3/BOJ_11279.cpp
@@ -1,58 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
-
-vector<int> heap;
-
-void insert_heap(int n)
-{
-	int cur = heap.size();
-	
-	heap.push_back(n);
-	
-	while(heap[cur]>heap[cur/2])
-	{
-		swap(heap[cur],heap[cur/2]);
-		cur/=2;
-	}
-}
-
-void delete_heap()
-{
-	int cur = 0;
-	
-	swap(heap[0], heap[heap.size()-1]);
-	heap.pop_back();
-	
-	if(heap.size()>2)
-	{
-		while(heap[cur] < max(heap[cur*2+1],heap[cur*2+2]))
-		{
-			if(heap[cur*2+1] >= heap[cur*2+2])
-			{
-				swap(heap[cur], heap[cur*2+1]);
-				cur*=2+1;
-			}
-			else
-			{
-				swap(heap[cur], heap[cur*2+2]);
-				cur*=2+2;
-			}
-		}
-	}
-	
-	else if(heap.size()==2)
-	{
-		if(heap[0] < heap[1])
-			swap(heap[0], heap[1]);
-	}
-}
 
 int main(void)
 {
 	int N, x;
+	priority_queue<int> heap;
 	
 	cin>>N;
 	
@@ -67,12 +21,12 @@ int main(void)
 			
 			else
 			{
-				cout<<heap[0]<<'\n';
-				delete_heap();
+				cout<<heap.top()<<'\n';
+				heap.pop();
 			}
 		}
 		
 		else
-			insert_heap(x);
+			heap.push(x);
 	}
 }
