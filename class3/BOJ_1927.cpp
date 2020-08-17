1e8f06b23ee9371ab9dfebca442f8013cf7725be
Sangwon Park <harin.luna@gmail.com>
2020-08-17 19:21:30 +0900

    Update BOJ_1927.cpp

--- class3/BOJ_1927.cpp
+++ class3/BOJ_1927.cpp
@@ -1,15 +1,62 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+vector<int> heap;
+
+void insert_heap(int n)
+{
+	int cur = heap.size();
+	
+	heap.push_back(n);
+	
+    while(cur>0 && heap[cur]<heap[(cur-1)/2])
+	{
+		swap(heap[cur],heap[(cur-1)/2]);
+		cur=(cur-1)/2;
+	}
+}
+
+void delete_heap()
+{
+	int cur = 0;
+	
+	swap(heap[0], heap[heap.size()-1]);
+	heap.pop_back();
+	
+	while(heap.size()>cur*2+2)
+	{
+		if(heap[cur] < min(heap[cur*2+1],heap[cur*2+2]))
+		   break;
+		   
+		if(heap[cur*2+1] <= heap[cur*2+2])
+		{
+			swap(heap[cur], heap[cur*2+1]);
+			cur=cur*2+1;
+		}
+		   
+		else
+		{
+			swap(heap[cur], heap[cur*2+2]);
+			cur=cur*2+2;
+		}
+	}
+	
+	if(heap.size()>cur*2+1)
+	{
+		if(heap[cur] > heap[cur*2+1])
+			swap(heap[cur], heap[cur*2+1]);
+	}
+}
+
 int main(void)
 {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+    
 	int N, x;
-	priority_queue<int, vector<int>, greater<int>> heap;
 	
 	cin>>N;
 	
@@ -24,12 +71,12 @@ int main(void)
 			
 			else
 			{
-				cout<<heap.top()<<'\n';
-				heap.pop();
+				cout<<heap[0]<<'\n';
+				delete_heap();
 			}
 		}
 		
 		else
-			heap.push(x);
+			insert_heap(x);
 	}
 }
