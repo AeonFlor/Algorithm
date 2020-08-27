c1065bcd10e9658dcdceab8081f452c9edf1b227
Sangwon Park <harin.luna@gmail.com>
2020-08-27 11:58:52 +0900

    Update BOJ_2263.cpp

--- class4/BOJ_2263.cpp
+++ class4/BOJ_2263.cpp
@@ -8,44 +8,34 @@ using namespace std;
 vector<int> inorder, preorder;
 stack<int> postorder;
 
-void build_pre(int inPos, int prePos, int start, int end)
+void build_pre(int prePos, int start, int end)
 {
 	if(postorder.empty())
 		return;
 	
+	if(start==end)
+		return;
+	
 	int root = postorder.top();
-	int pos = find(inorder.begin()+inPos+1, inorder.begin()+end, root) - inorder.begin();
+	postorder.pop();
 	
-	cout<<"[Recursion] root is "<<root<<" ("<<start<<", "<<end<<") -> PrePos : "<<prePos<<'\n';
-	cout<<"Right pos is "<<pos<<", and end is "<<end<<'\n';
+	int pos = find(inorder.begin()+start, inorder.begin()+end, root) - inorder.begin();
 	
-	if(pos!=end)
-	{
-		cout<<"[R] root is "<<root<<" and prePos is "<<prePos<<'\n';
-		preorder.insert(preorder.begin()+prePos+1, root);
-		postorder.pop();
-		
-		build_pre(pos, prePos+1, inPos+1, end);
-		cout<<"[R - out]\n";
-		root = postorder.top();
-	}
+	if(pos==end)
+		return;
 	
-	pos = find(inorder.begin()+start, inorder.begin()+inPos, root) - inorder.begin();
-	cout<<"Left pos is "<<pos<<", and inPos is "<<inPos<<'\n';
+	preorder.insert(preorder.begin()+prePos, root);
 	
-	if(pos!=inPos)
-	{
-		cout<<"[L] root is "<<root<<" and prePos is "<<prePos<<'\n';
-		preorder.insert(preorder.begin()+prePos+1,root);
-		postorder.pop();
-		
-		build_pre(pos, prePos, start, inPos);
-		cout<<"[L - out]\n";
-	}
+	build_pre(prePos+1, pos+1, end);
+	
+	build_pre(prePos+1, start, pos);
 }
 
 int main(void)
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
 	int n, input;
 	
 	cin>>n;
@@ -62,18 +52,9 @@ int main(void)
 		postorder.push(input);
 	}
 	
-	preorder.push_back(postorder.top());
-	postorder.pop();
-	
-	build_pre(find(inorder.begin(), inorder.end(), preorder.front()) - inorder.begin(), 0, 0, n);
+	build_pre(0, 0, n);
 	
 	for(int i=0; i<preorder.size(); ++i)
 		cout<<preorder[i]<<' ';
 	cout<<'\n';
 }
-
-/*
-13
-1 3 2 4 5 6 7 8 9 10 11 12 13
-1 2 3 6 5 4 9 8 11 13 12 10 7
-*/
