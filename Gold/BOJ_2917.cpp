f77f8564da06e9a90fffaa4ce75af861b46154cf
Sangwon Park <harin.luna@gmail.com>
2020-09-05 12:33:18 +0900

    Update BOJ_2917.cpp

--- Gold/BOJ_2917.cpp
+++ Gold/BOJ_2917.cpp
@@ -2,31 +2,63 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <tuple>
 #include <cmath>
 
 using namespace std;
+
+int N, M, J_x, J_y, V_x, V_y, value, sol;
+vector<pair<int,int>> tree;
+
+int minField[502][502];
+bool visited[502][502] = {false,};
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+
+int bfs(int x, int y, int ans)
+{
+	if(minField[x][y] == -10000)
+		return 0;
+	
+	priority_queue<tuple<int,int,int>> q;
+	
+	int minDist = 2500001;
+	
+	ans = max(ans, minField[x][y]);
+	//cout<<"coor("<<x<<","<<y<<") -> ans is "<<ans<<'\n';
+	
+	for(int i=0; i<4; ++i)
+		q.push({-minField[x+dx[i]][y+dy[i]], x+dx[i], y+dy[i]});
+	
+	while(!q.empty())
+	{
+		int nextDist, next_x,next_y;
+		tie(nextDist, next_x, next_y) = q.top();
+		q.pop();
+		
+		if(visited[next_x][next_y])
+			continue;
+		
+		visited[next_x][next_y] = true;
+		
+		minDist = min(minDist, nextDist + bfs(next_x, next_y, ans));
+	}
+	
+	sol = ans;
+	
+	return minDist;
+}
 
 int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
-	int N, M, J_x, J_y, V_x, V_y, ans, value;
-	vector<pair<int,int>> tree;
-	priority_queue<pair<pair<int,int>, pair<int,int>>> q;
-	
-	int minField[502][502];
-	int dist[502][502];
-	int dx[4] = {0,1,0,-1};
-	int dy[4] = {1,0,-1,0};
-	
-	fill(&minField[0][0], &minField[501][501], -1001);
-	fill(&dist[0][0], &dist[501][501], 1000);
-	
 	cin>>N>>M;
 	
 	char input;
-	
+	fill(&minField[0][0], &minField[501][501], -10000);
+
 	for(int i=1; i<=N; ++i)
 	{
 		for(int j=1; j<=M; ++j)
@@ -62,41 +94,7 @@ int main(void)
 			}
 	}
 	
-	dist[V_x][V_y] = minField[V_x][V_y];
-	ans = minField[V_x][V_y];
-	
-	q.push({{ans, -dist[V_x][V_y]},{V_x, V_y}});
-	
-	while(!q.empty())
-	{
-		int here_x = q.top().second.first;
-		int here_y = q.top().second.second;
-		int cost = -q.top().first.second;
-		q.pop();
-		
-		if(here_x == J_x && here_y == J_y)
-			break;
-		
-		else if(dist[here_x][here_y] < cost)
-			continue;
-		
-		for(int i=0; i<4; ++i)
-		{
-			int thereCost = minField[here_x + dx[i]][here_y + dy[i]];
-			
-			if(thereCost != -1001)
-			{
-				if(thereCost + cost < dist[here_x + dx[i]][here_y + dy[i]])
-				{
-					cout<<"("<<here_x + dx[i]<<", "<<here_y + dy[i]<<") is renewed.\n";
-					dist[here_x + dx[i]][here_y + dy[i]] = thereCost + cost;
-					if(ans < thereCost)
-						ans = thereCost;
-					q.push({{thereCost, dist[here_x + dx[i]][here_y + dy[i]]}, {here_x + dx[i],here_y + dy[i]}});
-				}
-			}
-		}
-	}
-	
-	cout<<1000-ans<<'\n';
+	bfs(V_x, V_y, -1);
+
+	cout<<1000-sol<<'\n';
 }
