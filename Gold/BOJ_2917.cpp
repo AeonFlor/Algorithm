759c7a3042a9321fb349ebc8aaad3b5071791bf2
Sangwon Park <harin.luna@gmail.com>
2020-09-05 10:51:44 +0900

    Update BOJ_2917.cpp

--- Gold/BOJ_2917.cpp
+++ Gold/BOJ_2917.cpp
@@ -8,39 +8,41 @@ using namespace std;
 
 int main(void)
 {
-	int N, M, J_x, J_y, V_x, V_y, ans;
-	vector<pair<int,int>> tree;
-	priority_queue<pair<int, pair<int,int>>> q;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 	
-	char field[502][502];
+	int N, M, J_x, J_y, V_x, V_y, ans, value;
+	vector<pair<int,int>> tree;
+	priority_queue<pair<pair<int,int>, pair<int,int>>> q;
+	
 	int minField[502][502];
 	int dist[502][502];
-	
 	int dx[4] = {0,1,0,-1};
 	int dy[4] = {1,0,-1,0};
 	
-	fill(&field[0][0], &field[501][501], 'x');
 	fill(&minField[0][0], &minField[501][501], -1001);
 	fill(&dist[0][0], &dist[501][501], 1000);
 	
 	cin>>N>>M;
 	
+	char input;
+	
 	for(int i=1; i<=N; ++i)
 	{
 		for(int j=1; j<=M; ++j)
 		{
-			cin>>field[i][j];
+			cin>>input;
 			
-			if(field[i][j]=='+')
+			if(input=='+')
 				tree.push_back({i,j});
 			
-			else if(field[i][j]=='J')
+			else if(input=='J')
 			{
 				J_x = j;
 				J_y = i;
 			}
 			
-			else if(field[i][j]=='V')
+			else if(input=='V')
 			{
 				V_x = j;
 				V_y = i;
@@ -50,41 +52,32 @@ int main(void)
 	
 	for(int iter = 0; iter<tree.size(); ++iter)
 	{
-		//cout<<"[Transformation] "<<tree[iter].first<<", "<<tree[iter].second<<'\n';
 		for(int i=1; i<=N; ++i)
 			for(int j=1; j<=M; ++j)
 			{
-				int value = 1000 - abs(tree[iter].first - i) - abs(tree[iter].second - j);
-				//cout<<value<<" and "<<minField[i][j]<<'\n';
+				value = 1000 - abs(tree[iter].first - i) - abs(tree[iter].second - j);
+				
 				if(minField[i][j] < value)
 					minField[i][j] = value;
 			}
 	}
 	
-	/*
-	for(int i=1; i<=N; ++i)
-	{
-		for(int j=1; j<=M; ++j)
-		{
-			cout<<minField[i][j]<<' ';
-		}
-		cout<<'\n';
-	}
-	*/
-	
 	dist[V_x][V_y] = minField[V_x][V_y];
 	ans = minField[V_x][V_y];
 	
-	q.push({-dist[V_x][V_y],{V_x, V_y}});
+	q.push({{ans, -dist[V_x][V_y]},{V_x, V_y}});
 	
 	while(!q.empty())
 	{
 		int here_x = q.top().second.first;
 		int here_y = q.top().second.second;
-		int cost = -q.top().first;
+		int cost = -q.top().first.second;
 		q.pop();
 		
-		if(dist[here_x][here_y] < cost)
+		if(here_x == J_x && here_y == J_y)
+			break;
+		
+		else if(dist[here_x][here_y] < cost)
 			continue;
 		
 		for(int i=0; i<4; ++i)
@@ -99,7 +92,7 @@ int main(void)
 					dist[here_x + dx[i]][here_y + dy[i]] = thereCost + cost;
 					if(ans < thereCost)
 						ans = thereCost;
-					q.push({thereCost + cost, {here_x + dx[i],here_y + dy[i]}});
+					q.push({{thereCost, dist[here_x + dx[i]][here_y + dy[i]]}, {here_x + dx[i],here_y + dy[i]}});
 				}
 			}
 		}
