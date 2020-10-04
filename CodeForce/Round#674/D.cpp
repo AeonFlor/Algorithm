085b4975fda7ae8d1088c11bae814eac97abf7dc
Sangwon Park <harin.luna@gmail.com>
2020-10-04 22:56:50 +0900

    Update D.cpp

--- CodeForce/Round#674/D.cpp
+++ CodeForce/Round#674/D.cpp
@@ -1,39 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <set>
 
 using namespace std;
-
-vector<int> arr;
-int ans=0;
-
-int countInsert(int start, int end)
-{
-	bool check = false;
-	
-	for(int i=start; i<end; ++i)
-	{
-		long long sum = 0LL;
-		
-		for(int j=i; j<end; ++j)
-		{
-			sum += arr[j];
-			
-			if(sum == 0)
-			{
-				check = true;
-				
-				if(!countInsert(i+1,j))
-					++ans;
-				
-				i = j-1;
-				
-				break;
-			}
-		}
-	}
-	
-	return check;
-}
 
 int main(void)
 {
@@ -41,16 +9,32 @@ int main(void)
 	cin.tie(0);
 	
 	int n, input;
+	long long tot=0 ,cnt=0;
+	
+	set<long long> sum;
 	
 	cin>>n;
+	
+	sum.insert(0);
 	
 	for(int i=0; i<n; ++i)
 	{
 		cin>>input;
-		arr.push_back(input);
+		
+		tot += input;
+		
+		if(sum.find(tot) != sum.end())
+		{
+			sum.clear();
+			sum.insert(0);
+			tot = input;
+			sum.insert(tot);
+			++cnt;
+		}
+		
+		else
+			sum.insert(tot);
 	}
 	
-	countInsert(0, n);
-	
-	cout<<ans<<'\n';
+	cout<<cnt<<'\n';
 }
