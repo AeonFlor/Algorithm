b0817a74662ec205d3a5106687f62af530abd169
AeonFlor <harin.luna@gmail.com>
2021-09-04 22:32:53 +0900

    2021-09-04 Solved Problems

--- BOJ/알고리즘 스터디/17142.cpp
+++ BOJ/알고리즘 스터디/17142.cpp
@@ -11,31 +11,11 @@ int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { -1, 0, 1, 0 };
 
 vector<vector<int>> map;
-vector<vector<char>> mirror;
 vector<vector<bool>> visited;
 vector<pair<int, int>> virus, active;
 
-void print(int time)
-{
-	cout << "-> " << time << '\n';
-
-	for (int i = 1; i < N + 1; ++i)
-	{
-		for (int j = 1; j < N + 1; ++j)
-		{
-			cout << mirror[i][j] << ' ';
-		}
-
-		cout << '\n';
-	}
-
-	cout << '\n';
-}
-
 void bfs()
 {
-	mirror.assign(N + 2, vector<char>(N + 2, '-'));
-
 	int y, x, time;
 	queue<tuple<int, int, int>> q;
 
@@ -49,14 +29,8 @@ void bfs()
 
 	visited.assign(N + 2, vector<bool>(N + 2, false));
 
-	for (int i = 0; i < virus.size(); ++i)
-	{
-		mirror[virus[i].first][virus[i].second] = '*';
-	}
-
 	for (int i = 0; i < active.size(); ++i)
 	{
-		mirror[active[i].first][active[i].second] = '0';
 		q.push({ active[i].first, active[i].second, 0 });
 		visited[active[i].first][active[i].second] = true;
 	}
@@ -68,19 +42,17 @@ void bfs()
 
 		for (int i = 0; i < 4; ++i)
 		{
-			if (!visited[y + dy[i]][x + dx[i]] && map[y + dy[i]][x + dx[i]] == 0)
+			if (!visited[y + dy[i]][x + dx[i]] && (map[y + dy[i]][x + dx[i]] == 0 || map[y + dy[i]][x + dx[i]] == 2))
 			{
 				q.push({ y + dy[i], x + dx[i], time + 1 });
 				visited[y + dy[i]][x + dx[i]] = true;
-				mirror[y + dy[i]][x + dx[i]] = time + 1 + '0';
 				
-				if (--cnt_copy == 0)
+				if (map[y + dy[i]][x + dx[i]] == 0 && --cnt_copy == 0)
 				{
 					while (!q.empty())
 						q.pop();
 
 					ans = (ans < time + 1) ? ans : time + 1;
-					print(time + 1);
 				}
 			}
 		}
@@ -137,3 +109,6 @@ int main(void)
 	else
 		cout << ans << '\n';
 }
+
+// 시간 적게 쓰인 소스 : https://www.acmicpc.net/source/32833118
+// 분석해 볼 소스 : https://www.acmicpc.net/source/32303370
