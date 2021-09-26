448149219ab0c71e3dabf15320e41259dedb8151
AeonFlor <harin.luna@gmail.com>
2021-09-26 14:55:48 +0900

    solve BOJ 1088

--- BOJ/1088.cpp
+++ BOJ/1088.cpp
@@ -1,25 +1,28 @@
-// 00:35
+// 00:35 ~ 01:40
+// 14:00 ~ 14:55
 
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
 
 int main(void)
 {
 	int N, M;
-	double input,  min_diff, temp, div;
+	double input,  min_diff;
+	tuple<double, double, int> temp, div;
 
-	list<double> cake;
-	list<double>::iterator first, last, pos;
+	list<tuple<double, double, int>> cake;
+	list<tuple<double, double, int>>::iterator first, last, pos;
 
 	cin >> N;
 
 	for (int i = 0; i < N; ++i)
 	{
 		cin >> input;
-		cake.push_back(input);
+		cake.push_back({ input, input, 1 });
 	}
 
 	cake.sort();
@@ -28,7 +31,7 @@ int main(void)
 	last = cake.end();
 	--last;
 
-	min_diff = *last - *first;
+	min_diff = get<0>(*last) - get<0>(*first);
 
 	cin >> M;
 
@@ -38,51 +41,20 @@ int main(void)
 		--last;
 		cake.pop_back();
 
-		/*
-		// 가장 작은 조각과 같은 크기와 나머지로 잘랐을 경우, 나머지가 가장 큰 값보다 큰 지 확인.
-		if (temp - *first > *last)
-		{
-			// 가장 작은 조각에 나머지와 가장 큰 값의 차이만큼 더한 무게로 나눈다. div 가 새롭게 나뉘어 진 경우 중 작은 조각이다.
-			// 결과는 *last 와 (*first + 차이)이다.
-			div = *first + (temp - *first - *last);
+		get<0>(temp) = get<1>(temp) / ++get<2>(temp);
 
-			// 작은 조각이 가장 큰 값보다 커진다면, 차이가 최소가 되도록 다시 나눈다. (작은 조각이 큰 조각보다 더 커지는 건 상관없지만 원래의 목적인 가장 큰 값보다 커지면 안 됨.)
-			if (div > *last)
-			{
-				// temp 의 중간값이어야 하는 이유는 하나가 조금이라도 더 작아지면, 다른 게 커져 결국 범위가 넓어진다. 범위를 줄이는 게 목적. 
-				div = temp / 2;
-			}
-		}
-
-		else
-		{
-			if (temp - *first < *first)
-			{
-				div = temp / 2;
-			}
-
-			else
-			{
-				div = *first;
-			}
-		}
-		*/
-
-		div = temp / 2;
-
-		pos = upper_bound(cake.begin(), cake.end(), div);
-		cake.insert(pos, div);
-
-		pos = upper_bound(cake.begin(), cake.end(), temp - div);
-		cake.insert(pos, temp-div);
+		pos = upper_bound(cake.begin(), cake.end(), temp);
+		cake.insert(pos, temp);
 
 		first = cake.begin();
 		last = cake.end();
 		--last;
 
-		min_diff = (min_diff < *last - *first) ? min_diff : *last - *first;
+		min_diff = (min_diff < get<0>(*last) - get<0>(*first)) ? min_diff : get<0>(*last) - get<0>(*first);
 	}
 
+	cout << fixed;
+	cout.precision(30);
 	cout << min_diff << '\n';
 
 	return 0;
