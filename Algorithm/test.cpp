ec0b36081a91eace51b29b1d24fa7e0588469703
AeonFlor <harin.luna@gmail.com>
2024-02-20 18:31:22 +0900

    settting: Set up environment for solving in familiar Visual Studio

---
+++ Algorithm/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	// remove below line when code copy
+	freopen("input.txt", "r", stdin);
+
+	string a;
+
+	cin >> a;
+	cout << a;
+}
